c4e530e958eff271e0f3b2b96ea3656fedaf2d62
Benji42 <cheezyanimations@gmail.com>
2024-08-19 02:04:04 -0400

    add hawaiian file

---
+++ hawaiian.py
@@ -0,0 +1,32 @@
+from washitsu import *
+from washitsu.rules import *
+
+segments = ipa(
+    "i",
+    "u",
+    "e",
+    "u",
+    "o",
+    "a",
+    "m",
+    "n",
+    "p",
+    "k",
+    "ʔ",
+    "h",
+    "w",
+    "l",
+)
+
+for i in range(0, 10):
+    word_length = random.randint(1, 5)
+    (
+        Word(
+            [a(segments,
+                [probability(-syllabic, 0.8)],
+                [+syllabic],
+                [],
+               ) for a in [syllable] * word_length]
+        )
+        .show()
+    )
